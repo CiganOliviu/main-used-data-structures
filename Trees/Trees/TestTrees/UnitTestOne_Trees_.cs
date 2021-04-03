5d490f7310b2cec3644e4b3735843d071db114ab
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-04-03 18:46:43 +0300

    Setup Trees Skeleton

---
+++ Trees/Trees/TestTrees/UnitTestOne_Trees_.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestTrees
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test__()
+        {
+            Assert.Pass();
+        }
+    }
+}
