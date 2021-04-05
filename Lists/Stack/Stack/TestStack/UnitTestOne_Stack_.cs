d29be3c8873b7c5e313e98a0d52339d619eef564
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-04-05 23:23:13 +0300

    Create Stack data structure

---
+++ Lists/Stack/Stack/TestStack/UnitTestOne_Stack_.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestStack
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test__()
+        {
+            Assert.Pass();
+        }
+    }
+}
