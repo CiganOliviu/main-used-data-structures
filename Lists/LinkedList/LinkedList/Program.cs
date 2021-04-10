f2ee37e47bac77a005fa9a2ff144eb47d6d2037a
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-04-10 22:39:14 +0300

    Update Porgram.cs

--- Lists/LinkedList/LinkedList/Program.cs
+++ Lists/LinkedList/LinkedList/Program.cs
@@ -2,11 +2,167 @@
 
 namespace LinkedList
 {
+    public class LinkedListDataStructure
+    {
+        public static Node first;
+        public static Node last;
+            
+        public class Node
+        {
+            public int key;
+            public Node next;
+                
+            public Node(int data) { key = data; next = null; }
+        }
+
+        public void insertFirst(int key)
+        {
+            var newNode = new Node(key);
+
+            if (first == null)
+                last = newNode;
+            else
+                newNode.next = first;
+
+            first = newNode;
+        }
+
+        public void insertlast(int key)
+        {
+            var newNode = new Node(key);
+
+            if (last == null)
+                first = newNode;
+            else
+                last.next = newNode;
+
+            last = newNode;
+        }
+
+        public Node searchNode(int givenKey)
+        {
+            var currentNode = first;
+
+            while (currentNode != null)
+            {
+                if (currentNode.key == givenKey)
+                    break;
+                
+                currentNode = currentNode.next;
+            }
+
+            return currentNode;
+        }
+
+        public void insertAfterKey(int afterKey, int givenKey)
+        {
+            var currentNode = searchNode(afterKey);
+            var newNode = new Node(givenKey);
+
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+
+            if (currentNode == last)
+                last = newNode;
+        }
+
+        public void deleteFirst()
+        {
+            if (first == null)
+                return;
+
+            first = first.next;
+        }
+
+        public void deleteLast()
+        {
+            var toDelete = first;
+            Node previousNode = null;
+            
+            if (toDelete == null)
+                return;
+
+            while (toDelete != last)
+            {
+                previousNode = toDelete;
+                toDelete = toDelete.next;
+            }
+
+            previousNode.next = null;
+            last = previousNode;
+        }
+        
+        public void deleteGivenKey(int givenKey)
+        {
+            var toDelete = first;
+            Node previousNode = null;
+            
+            if (toDelete == null)
+                return;
+
+            while (toDelete != null)
+            {
+                if (toDelete.key == givenKey)
+                    break;
+                
+                previousNode = toDelete;
+                toDelete = toDelete.next;
+            }
+
+            if (toDelete == first)
+            {
+                first = first.next;
+
+                if (first == null)
+                    last = null;
+            }
+            else
+            {
+                previousNode.next = toDelete.next;
+
+                if (toDelete == last)
+                    last = previousNode;
+            }
+        }
+
+        public void printList()
+        {
+            Console.WriteLine("My list is :");
+
+            var currentNode = first;
+
+            while (currentNode != null)
+            {
+                Console.Write(currentNode.key + " ");
+                currentNode = currentNode.next;
+            }
+        }
+    }
+    
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myList = new LinkedListDataStructure();
+            
+            myList.insertFirst(6);
+            myList.insertFirst(7);
+            
+            myList.insertlast(3);
+            myList.insertlast(5);
+            
+            myList.insertAfterKey(6, 8);
+            myList.insertAfterKey(3, 9);
+            
+            myList.printList();
+            
+            Console.WriteLine();
+            
+            myList.deleteFirst();
+            myList.deleteLast();
+            myList.deleteGivenKey(8);
+            
+            myList.printList();
         }
     }
 }
